552cdc16ac43a41c1a844c783e874a5a45b54b04
Rajat Bansal <65078224+rajatbansal01@users.noreply.github.com>
2021-09-14 14:31:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>This is a paragraph</p>
+      <small>This is a small text</small>
+    </>
+  );
+};
+
+export default App;
